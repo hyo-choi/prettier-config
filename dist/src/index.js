c67bc7692ff4686fbf55753d9f6cdb85166f482f
Hyojeong Choi <gywjddl001@naver.com>
2024-06-06 16:41:17 +0900

    :sparkles: Add prettier config source code

---
+++ dist/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: !0
+}), module.exports = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: !0,
+    quoteProps: "as-needed",
+    singleQuote: !1,
+    jsxSingleQuote: !1,
+    bracketSpacing: !0,
+    bracketSameLine: !1
+};
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  quoteProps: "as-needed",
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
+
+module.exports = config;
